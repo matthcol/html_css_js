3630f1cdfbb1fc2a5731cc1e1e282ebfefebcef4
Matthias Colin <matthias.colin@gmail.com>
2019-11-05 09:31:58 +0100

    first commit

---
+++ wanao.js
@@ -0,0 +1,10 @@
+function hello() {
+	console.log("Hello Tout le Monde !")
+}
+
+var compteur=0;
+var data = ["Bidart", "Biarritz", "Bayonne", "Anglet"];
+
+$(window).on('load', function() {
+	$("#a1").append("<p>2eparagraphe</p>")
+});
